8e92c492943d97dce0a8ff8de0a16c479b80705a
mehulmakavana <mehulmakavana52@gmail.com>
2021-06-16 17:23:00 +0530

    data

--- src/Pages/SendItem.js
+++ src/Pages/SendItem.js
@@ -22,20 +22,80 @@ export class CreateComplaints extends Component {
     
   }
 
+  componentDidMount() {
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: 'React POST Request Example' })
+    };
+    fetch('https://reqres.in/api/posts', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+}
 
-  handleSend(e) {
-    axios({
-      url: `http://localhost:8020/order/tokitchen/${this._id}/${this.props._id}`,
-      method: "PUT",
+
+componentDidMount() {
+  const recipeUrl = 'http://localhost:8080/api/recipes';
+  const postBody = {
+      type: "hot",
+      limit: 10
+  };
+  const requestMetadata = {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+          'Content-Type': 'application/json'
       },
-    })
-      .then((res) => res.data)
-      .then((data) => {
-        const { message } = data;
-        this.setState({ message });
+      body: JSON.stringify(postBody)
+  };
+
+  fetch(recipeUrl, requestMetadata)
+      .then(res => res.json())
+      .then(recipes => {
+          this.setState({ recipes });
       });
+    }
+
+    getAbandonedCart() {
+
+      let start_date = moment().subtract(6,'d').format('YYYY-MM-DD')
+      let end_date = moment().format('YYYY-MM-DD')
+      let measurement = 'value'
+      let where1 = JSON.stringify([["Campaign Name","=", this.campaignID]])
+      let where2 = JSON.stringify([["$attributed_message","=", 'Abandonded Cart: Email 1']])
+
+      axios.get('/api/v1/metric/' + this.metrics[3].id + '/export?start_date=' + start_date + '&end_date=' + end_date + '&measurement=' + measurement + '&where=' + where1 + '&where=' + where2).then(({data}) => {
+
+          for (let i = 0; i < data.results[0].data.length; i++) {
+             this.series[4]['data'].push(data.results[0].data[i].values[0])
+          }
+
+      });
+  },
+
+    async addTimes(ctx) {
+      return new Promise((resolve, reject) => {
+      let query = "INSERT INTO AdvisingTimes (id, Day, StartTime, EndTime, TimeBlock) VALUES (?, ?, ?, ?, ?);";
+      console.log('About to run this query.', query);
+          dbConnection.query(
+              {
+                  sql: query,
+                  values: [ctx.params.id, ctx.params.day, ctx.params.starttime, ctx.params.endtime, ctx.params.timeblock]
+              }, (error) => {
+                  if (error) {
+                      return reject(error);
+                  }
+              }
+  
+          )
+      }).catch(err => {
+          ctx.body = {
+              status: "Failed",
+              error: err,
+              user: null
+          };
+      });
+  
   }
 
   render() {
@@ -48,33 +108,7 @@ export class CreateComplaints extends Component {
             <td>Table Number</td>
           </table>
 
-          {this.state.people
-            .filter((order) => order.OrderIs === "Pending")
-            .map((order) => (
-              <div key={order._id}>
-                <table className="ccmt1">
-                  <tr>
-                    <td>{order._id}</td>
-                    <td>{order.name}</td>
-                  </tr>
-                </table>
-
-                {order.items.map((item) => (
-                  <div key={item._id}>
-                    <table className="ccmt1">
-                      <tr>
-                        <td>{item._id}</td>
-                        <td>
-                          <button onClick={(e) => this.handleSend(e)}>
-                            Send
-                          </button>
-                        </td>
-                      </tr>
-                    </table>
-                  </div>
-                ))}
-              </div>
-            ))}
+         
         </div>
       </div>
     );
--- src/Pages/send.js
+++ src/Pages/send.js
@@ -410,13 +410,7 @@ class PopupTable extends React.Component {
   }
   }
 
-  async menuitem() {
-    const url = "http://localhost:8020/menu/menues";
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({ cart: data.products });
-    this.searchArray = data;
-  }
+
 
   async handleClick(_id) {
     const url = "http://localhost:8020/menu/menu/" + _id;
@@ -426,15 +420,7 @@ class PopupTable extends React.Component {
     this.searchArray = data;
   }
 
-  handleName(e) {
-    let name = e.target.value;
-    this.setState({ name: name });
-  }
-
-  handleTable(e) {
-    let table = e.target.value;
-    this.setState({ table: table });
-  }
+ 
 
   handleUpload(e) {
     let name = this.state.name;
@@ -457,6 +443,92 @@ class PopupTable extends React.Component {
       .then((res) => res.data)
 
   }
+
+  async componentDidMount() {
+    const url = "http://localhost:8020/order/getorders";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ people: data.orders, loading: false, _id : data.orders._id});
+    this.searchArray = data;
+    
+  }
+
+  componentDidMount() {
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: 'React POST Request Example' })
+    };
+    fetch('https://reqres.in/api/posts', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+}
+
+
+componentDidMount() {
+  const recipeUrl = 'http://localhost:8080/api/recipes';
+  const postBody = {
+      type: "hot",
+      limit: 10
+  };
+  const requestMetadata = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postBody)
+  };
+
+  fetch(recipeUrl, requestMetadata)
+      .then(res => res.json())
+      .then(recipes => {
+          this.setState({ recipes });
+      });
+    }
+
+    getAbandonedCart() {
+
+      let start_date = moment().subtract(6,'d').format('YYYY-MM-DD')
+      let end_date = moment().format('YYYY-MM-DD')
+      let measurement = 'value'
+      let where1 = JSON.stringify([["Campaign Name","=", this.campaignID]])
+      let where2 = JSON.stringify([["$attributed_message","=", 'Abandonded Cart: Email 1']])
+
+      axios.get('/api/v1/metric/' + this.metrics[3].id + '/export?start_date=' + start_date + '&end_date=' + end_date + '&measurement=' + measurement + '&where=' + where1 + '&where=' + where2).then(({data}) => {
+
+          for (let i = 0; i < data.results[0].data.length; i++) {
+             this.series[4]['data'].push(data.results[0].data[i].values[0])
+          }
+
+      });
+  },
+
+    async addTimes(ctx) {
+      return new Promise((resolve, reject) => {
+      let query = "INSERT INTO AdvisingTimes (id, Day, StartTime, EndTime, TimeBlock) VALUES (?, ?, ?, ?, ?);";
+      console.log('About to run this query.', query);
+          dbConnection.query(
+              {
+                  sql: query,
+                  values: [ctx.params.id, ctx.params.day, ctx.params.starttime, ctx.params.endtime, ctx.params.timeblock]
+              }, (error) => {
+                  if (error) {
+                      return reject(error);
+                  }
+              }
+  
+          )
+      }).catch(err => {
+          ctx.body = {
+              status: "Failed",
+              error: err,
+              user: null
+          };
+      });
+  
+  }
+
   handleSend(e) {
     axios({
       url: `http://localhost:8020/order/tokitchen/${this._id}/${this.props._id}`,
@@ -476,20 +548,6 @@ class PopupTable extends React.Component {
     this.setState({ value: event.target.value });
 };
 
-handleBook() {
-    alert("Your Detail is Saved!")
-}
-
-handleSubmit(date) {
-    this.setState({
-        startDate: date
-    })
-}
-
-onFormSubmit(e) {
-    e.preventDefault();
-    console.log(this.state.startDate)
-}
 
 
   render() {
@@ -563,91 +621,7 @@ onFormSubmit(e) {
                     <div key={person._id}>
                       <div className="cardItem-menus">
                         <div classname="image">
-                          <img
-                            width="230px"
-                            height="230px"
-                            src={person.imageUrl}
-                            alt=""
-                          />
-                        </div>
-                        <div className="content-data">
-                          <div className="menu-data">{person.name}</div>
-                          <div className="menu-description">
-                            Description :- {person.description}
-                          </div>
-                          <div className="price">
-                            <div className="menu-price">
-                              price :- {person.originalPrice} ₹{" "}
-                            </div>
-                          </div>
-                          <div>
-                            <div className="priority-set">
-                              <button
-                                type="button"
-                                className="priority-btn"
-                                onClick={this.incrementCount}
-                              >
-                                +
-                              </button>
-                              <div classNam="p-data">
-                                Priority : {this.state.priority}
-                              </div>
-                              <button
-                                type="button"
-                                className="priority-btn"
-                                onClick={this.DecrementCount}
-                              >
-                                -
-                              </button>
-                            </div>
-
-                            <div className="Quantity-set">
-                              <button
-                                type="button"
-                                className="Quantity-btn"
-                                onClick={this.incrementQTY}
-                              >
-                                +
-                              </button>
-                              <div className="q-data">
-                                Quantity : {this.state.quantity}
-                              </div>
-                              <button
-                                type="button"
-                                className="Quantity-btn"
-                                onClick={this.DecrementQTY}
-                              >
-                                -
-                              </button>
-                            </div>
-
-                            <div className="Order-Note" htmlFor="Order-Name">
-                              Add Notes
-                            </div>
-                            <div>
-                              <input
-                                className="input-notes"
-                                type="text"
-                                name="name"
-                                placeholder="Enter Order Note"
-                                onChange={(e) => this.handleName(e)}
-                              />
-                            </div>
-                          </div>
-                          <button
-                            className="addCart"
-                            onClick={() =>
-                              this.addCart(
-                                person._id,
-                                this.state.priority,
-                                this.state.quantity,
-                                this.state.name
-                              )
-                            }
-                          >
-                            Add to Cart
-                          </button>
-                        </div>
+                        
                       </div>
                     </div>
                   ))}
@@ -656,8 +630,7 @@ onFormSubmit(e) {
               </div>
             ))}
           </div>
-        </div>
-      </div>
+     
     );
   }
 }
